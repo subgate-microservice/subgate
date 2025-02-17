d1c01a8c1498b4e787c662923b0d4d557a0fa81a
barma <barmatey@gmail.com>
2025-02-17 10:50:41 +0300

    another way

--- backend/tests/test_concurrency/test_concurrency_with_plan.py
+++ backend/tests/test_concurrency/test_concurrency_with_plan.py
@@ -3,32 +3,27 @@ import asyncio
 import pytest
 
 from backend.bootstrap import container
-from tests.fake_data import create_plan
+from tests.fakes import simple_plan
 
 
 @pytest.mark.asyncio
-async def test_concurrency_with_update():
-    # Before
-    plan = create_plan()
-    async with container.unit_of_work_factory().create_uow() as uow:
-        await uow.plan_repo().add_one(plan)
-        await uow.commit()
+async def test_concurrency_with_update(simple_plan):
+    plan = simple_plan
 
-    # Test
     async def first_started_second_finished():
         async with container.unit_of_work_factory().create_uow() as uw:
             target = await uw.plan_repo().get_one_by_id(plan.id)
             await asyncio.sleep(1)
-            updated = target.model_copy(update={"price": 222})
-            await uw.plan_repo().update_one(updated)
+            target.price = 222
+            await uw.plan_repo().update_one(target)
             await uw.commit()
 
     async def second_started_first_finished():
         await asyncio.sleep(0.5)
         async with container.unit_of_work_factory().create_uow() as uw:
             target = await uw.plan_repo().get_one_by_id(plan.id)
-            updated = target.model_copy(update={"title": "Updated"})
-            await uw.plan_repo().update_one(updated)
+            target.title = "Updated"
+            await uw.plan_repo().update_one(target)
             await uw.commit()
 
     task1 = first_started_second_finished()
