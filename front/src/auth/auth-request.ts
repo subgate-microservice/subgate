089e5d9a3f8afeb9d21b6bd84480f493b780c7df
barma <barmatey@gmail.com>
2025-03-03 18:05:15 +0300

    processing

--- front/src/auth/auth-request.ts
+++ front/src/auth/auth-request.ts
@@ -1,17 +1,4 @@
 import {AxiosResponse} from "axios";
-import {fiefAuth, fiefLogout} from "./fief.ts";
-
-export function getAuthHeaders() {
-    // const tokenInfo = {access_token: "FakeAuthToken"}
-    const tokenInfo = fiefAuth.getTokenInfo();
-
-    if (tokenInfo) {
-        return {
-            "Authorization": `Bearer ${tokenInfo.access_token}`,
-        }
-    }
-    return {}
-}
 
 export async function authRequest(f: (...x: any[]) => Promise<AxiosResponse>, ...args: any[]): Promise<AxiosResponse> {
     try {
@@ -20,14 +7,9 @@ export async function authRequest(f: (...x: any[]) => Promise<AxiosResponse>, ..
         console.error("Auth request failed")
         if (!err?.response?.ok) {
             if (err.response.status === 401 || err.response.status === 403) {
-                await fiefLogout()
                 throw Error("AuthError")
             }
         }
         throw err
     }
-}
-
-export async function maybeAuthRequest(f: (...x: any[]) => Promise<AxiosResponse>, ...args: any[]): Promise<AxiosResponse> {
-    return authRequest(f, ...args)
 }
--- front/src/axios-instanse.ts
+++ front/src/axios-instanse.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {getAuthHeaders} from "./auth/auth-request.ts";
 import {BACKEND_BASE_URL} from "../config.ts";
 
 export const axiosInstance = axios.create(
@@ -7,7 +6,6 @@ export const axiosInstance = axios.create(
         baseURL: BACKEND_BASE_URL,
         headers: {
             "Accept": "application/json",
-            ...getAuthHeaders(),
         },
         withCredentials: true,
     }
