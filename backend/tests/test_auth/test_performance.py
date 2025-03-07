b39db84032c175e303f1d2284ef1afbd167b34d4
barma <barmatey@gmail.com>
2025-03-07 12:51:11 +0300

    cache

--- backend/tests/test_auth/test_performance.py
+++ backend/tests/test_auth/test_performance.py
@@ -4,10 +4,12 @@ import datetime
 import pytest
 from loguru import logger
 
+from tests.conftest import apikey_client, client
+
 
 class TestApikeyAuthenticationPerformance:
     @pytest.mark.asyncio
-    async def test_get_plan_without_concurrency(self, apikey_client):
+    async def test_get_myself_without_concurrency(self, apikey_client):
         start = datetime.datetime.now()
         for i in range(1_000):
             response = await apikey_client.get(f"/users/me")
@@ -16,7 +18,7 @@ class TestApikeyAuthenticationPerformance:
         logger.debug(f"Total time is {(end - start)}")
 
     @pytest.mark.asyncio
-    async def test_get_plan_without_with_concurrency(self, apikey_client):
+    async def test_get_myself_without_with_concurrency(self, apikey_client):
         start = datetime.datetime.now()
 
         tasks = []
@@ -26,5 +28,16 @@ class TestApikeyAuthenticationPerformance:
             tasks.append(task)
         await asyncio.gather(*tasks)
 
+        end = datetime.datetime.now()
+        logger.debug(f"Total time is {(end - start)}")
+
+
+class TestTokenAuthenticationPerformance:
+    @pytest.mark.asyncio
+    async def test_get_myself_without_concurrency(self, client):
+        start = datetime.datetime.now()
+        for i in range(1_000):
+            response = await client.get(f"/users/me")
+            response.raise_for_status()
         end = datetime.datetime.now()
         logger.debug(f"Total time is {(end - start)}")
